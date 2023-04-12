264b18a382fdcb703dde3efda1d0766836dede20
Gizmo <gizmo.dev.dev@gmail.com>
2023-04-12 13:17:20 +0300

    add utils with test

---
+++ src/utils/isSortedDescendant/isSortedDescendant.ts
@@ -0,0 +1,10 @@
+/**
+ * Функция проверяет, что массим отсортирован по неубыванию
+ * @param array - входной массив
+ */
+export const isSortedDescendant = (array: number[]): boolean => {
+  return array.every((item, index, a) => {
+    if (index === 0) return true;
+    return a[index - 1] >= item;
+  });
+};
